eb3a660bbc5916f92170a80cdea11a994a4604b0
Leandro Noguera <nogueraleandro@hotmail.com>
2020-04-08 15:02:23 -0300

    declaracion funcion factorial, calculos, impresion de resultados

--- TP_1/funciones.c
+++ TP_1/funciones.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int funcionSuma (int a, int b) {
+
+    int resultado;
+
+    resultado = a + b;
+
+    return resultado;
+}
+
+int funcionResta (int a, int b) {
+
+    int resultado;
+
+    resultado = a - b;
+
+    return resultado;
+}
+
+float funcionDivision (int a, int b) {
+
+    float resultado;
+
+    resultado = (float) a / b;
+
+    return resultado;
+}
+
+int funcionMultiplicacion (int a, int b) {
+
+    int resultado;
+
+    resultado = a * b;
+
+    return resultado;
+}
+
+int funcionFactorial (int num) {
+
+    int factorial=1;
+    int i;
+
+
+    for(i=num; i>1; i--) {
+
+    factorial=factorial*i;
+
+   }
+
+   return factorial;
+}
--- TP_1/main.c
+++ TP_1/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "funciones.h"
 
 int main()
 {
-    int opcion;
-    int operandoA;
-    int OperandoB;
+    int opcion, operandoA, operandoB;
+    int suma, resta, multiplicacion, factorialA, factorialB;
+    float division;
 
     do {
 
-       printf("Seleccione una opcion: ");
+       printf("\nSeleccione una opcion: ");
        printf("\n1. Ingresar el 1er operando: ");
        printf("\n2. Ingresar el 2do operando: ");
        printf("\n3. Realizar todas las operaciones: ");
@@ -25,9 +26,28 @@ int main()
             break;
             case 2:
             printf("\nIngrese 2do operando: ");
-            scanf("%d", &OperandoB);
+            scanf("%d", &operandoB);
             break;
-
+            case 3:
+            suma = funcionSuma(operandoA, operandoB);
+            resta = funcionResta(operandoA, operandoB);
+            division = funcionDivision(operandoA, operandoB);
+            multiplicacion = funcionMultiplicacion(operandoA, operandoB);
+            factorialA = funcionFactorial(operandoA);
+            factorialB = funcionFactorial(operandoB);
+            break;
+            case 4:
+            printf("\nEl resultado de %d + %d es %d", operandoA, operandoB, suma);
+            printf("\nEl resultado de %d - %d es %d", operandoA, operandoB, resta);
+            if(operandoB==0) {
+                printf("\nNo es posible dividir por 0");
+            } else {
+                printf("\nEl resultado de %d / %d es %f", operandoA, operandoB, division);
+            }
+            printf("\nEl resultado de %d * %d es %d", operandoA, operandoB, multiplicacion);
+            printf("\nEl factorial de %d es %d y el factorial de %d es %d",
+            operandoA, factorialA, operandoB, factorialB);
+            break;
         }
 
     } while(opcion!=5);
