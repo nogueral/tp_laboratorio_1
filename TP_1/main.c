1a9df09c33f37c581f0b9917d7f55ee86b48eb37
Leandro Noguera <nogueraleandro@hotmail.com>
2020-04-08 13:49:46 -0300

    Ingreso de operandos

--- TP_1/main.c
+++ TP_1/main.c
@@ -4,16 +4,31 @@
 int main()
 {
     int opcion;
+    int operandoA;
+    int OperandoB;
 
     do {
 
-       printf("Seleccione una opción: ");
+       printf("Seleccione una opcion: ");
        printf("\n1. Ingresar el 1er operando: ");
        printf("\n2. Ingresar el 2do operando: ");
        printf("\n3. Realizar todas las operaciones: ");
        printf("\n4. Informar resultados: ");
        printf("\n5. Salir: ");
        scanf("%d", &opcion);
+
+        switch(opcion) {
+
+            case 1:
+            printf("\nIngrese 1er operando: ");
+            scanf("%d", &operandoA);
+            break;
+            case 2:
+            printf("\nIngrese 2do operando: ");
+            scanf("%d", &OperandoB);
+            break;
+
+        }
 
     } while(opcion!=5);
 
