76780848414c32b3f85b742614792b71b176e3dd
Leandro Noguera <nogueraleandro@hotmail.com>
2020-04-17 11:54:13 -0300

    Modificacion del menu de opciones

--- TP1/TP1/main.c
+++ TP1/TP1/main.c
@@ -25,9 +25,9 @@ int main()
             printf("\n2. Ingresar el 2do operando: %.2f", operandoB);
        }
        if(flagC==1) {
-            printf("\n3. Realizar todas las operaciones: ");
+            printf("\n3. Realizar todas las operaciones: \n a) Calculo de suma \n b) Calculo de resta \n c) Calculo de division \n d) Calculo de multiplicacion \n e) Calculo de factorial");
        } else {
-            printf("\n3. Realizar todas las operaciones: Calculos completos");
+            printf("\n3. Realizar todas las operaciones: Calculos completos \n a) Calculo de suma \n b) Calculo de resta \n c) Calculo de division \n d) Calculo de multiplicacion \n e) Calculo de factorial");
        }
        printf("\n4. Informar resultados: ");
        printf("\n5. Salir: \n");
@@ -71,27 +71,27 @@ int main()
             break;
 
             case 4:
-            printf("\nEl resultado de %.2f + %.2f es %.2f", operandoA, operandoB, suma);
+            printf("\n a) El resultado de %.2f + %.2f es %.2f", operandoA, operandoB, suma);
 
-            printf("\nEl resultado de %.2f - %.2f es %.2f", operandoA, operandoB, resta);
+            printf("\n b) El resultado de %.2f - %.2f es %.2f", operandoA, operandoB, resta);
 
             if(operandoB==0) {
-                printf("\nNo es posible dividir por 0");
+                printf("\n c) No es posible dividir por 0");
             } else {
-                printf("\nEl resultado de %.2f / %.2f es %.2f", operandoA, operandoB, division);
+                printf("\n c) El resultado de %.2f / %.2f es %.2f", operandoA, operandoB, division);
             }
 
-            printf("\nEl resultado de %.2f * %.2f es %.2f", operandoA, operandoB, multiplicacion);
+            printf("\n d) El resultado de %.2f * %.2f es %.2f", operandoA, operandoB, multiplicacion);
 
             if(operandoA>-1 && validacionFactorialA==1) {
-                printf("\nEl factorial de %.2f es %.2f", operandoA, factorialA);
+                printf("\n e) El factorial de %.2f es %.2f", operandoA, factorialA);
             } else {
-                printf("\nNo se puede calcular el factorial de numeros negativos o decimales. Error al ingresar %.2f", operandoA);
+                printf("\n e) No se puede calcular el factorial de numeros negativos o decimales. Error al ingresar %.2f", operandoA);
             }
             if(operandoB>-1 && validacionFactorialB==1) {
-                printf("\nEl factorial de %.2f es %.2f\n", operandoB, factorialB);
+                printf("\n    El factorial de %.2f es %.2f\n", operandoB, factorialB);
             } else {
-                printf("\nNo se puede calcular el factorial de numeros negativos o decimales. Error al ingresar %.2f\n", operandoB);
+                printf("\n    No se puede calcular el factorial de numeros negativos o decimales. Error al ingresar %.2f\n", operandoB);
             }
             flagA=1;
             flagB=1;
