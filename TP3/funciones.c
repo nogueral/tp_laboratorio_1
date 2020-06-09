ce4f73039899d68d5b8caf8eaf4919ffc64190b8
Leandro Noguera <nogueraleandro@hotmail.com>
2020-06-08 21:17:35 -0300

    nombre de archivo binario en menu

--- TP3/funciones.c
+++ TP3/funciones.c
@@ -6,14 +6,14 @@ int MostrarMenu(void)
     printf("\n/****************************************************");
     printf("\nMenu:");
     printf("\n1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).");
-    printf("\n2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).");
+    printf("\n2. Cargar los datos de los empleados desde el archivo data.dat (modo binario).");
     printf("\n3. Alta de empleado");
     printf("\n4. Modificar datos de empleado");
     printf("\n5. Baja de empleado");
     printf("\n6. Listar empleados");
     printf("\n7. Ordenar empleados");
     printf("\n8. Guardar los datos de los empleados en el archivo data.csv (modo texto)..");
-    printf("\n9. Guardar los datos de los empleados en el archivo data.csv (modo binario).");
+    printf("\n9. Guardar los datos de los empleados en el archivo data.dat (modo binario).");
     printf("\n10. Salir");
     printf("\n*****************************************************/\n");
     scanf("%d", &opcion);
